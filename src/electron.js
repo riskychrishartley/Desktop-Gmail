184edcaed4f3fd9a560348c10a18b8107e70d0fb
Chris <a>
2020-01-29 21:06:42 -0500

    Support for Multiple Accounts and Security Updates

--- src/electron.js
+++ src/electron.js
@@ -1,17 +1,36 @@
 // Modules to control application life and create native browser window
 require("v8-compile-cache");
-const {app, BrowserWindow, Tray, Menu} = require('electron');
+const {app, BrowserWindow, Tray, Menu, session} = require('electron');
 const path = require('path');
 
 // Keep a global reference of the window object and tray item, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 let trayItem;
-
+let gmail = `https://mail.google.com`;
+let googleauth = `https://accounts.google.com`;
+let apis = 'https://*.google.com';
+let gstatic = "https://*.gstatic.com";
+let userAgent = {userAgent: 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/72.0'};
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', init);
+
+function init(){
+
+    // Define Content Security Policy before loading page
+    session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+        callback({
+            responseHeaders: {
+                ...details.responseHeaders,
+                'Content-Security-Policy': [`script-src-elem 'unsafe-inline' 'self' ${apis} ${gstatic} `]
+            }
+        })
+    });
+
+    createWindow();
+}
 
 
 function createWindow () {
@@ -33,15 +52,27 @@ function createWindow () {
 
     // Create the browser window.
     mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        show: false,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
+            nodeIntegration: false,
+            enableRemoteModule: false,
         }
     });
 
+    mainWindow.maximize();
+    mainWindow.show();
+    //mainWindow.webContents.openDevTools();
     // and load the index.html of the app.
-    mainWindow.loadURL(`https://gmail.com`);
+    mainWindow.loadURL(gmail, userAgent);
+
+
+    mainWindow.webContents.on('new-window', (e,url) => {
+        e.preventDefault();
+        if(url.includes(googleauth) || url.includes(gmail)) mainWindow.loadURL(url, userAgent);
+        else contentWindow(url);
+    });
+
+
 
     // hide function toolbar
     mainWindow.removeMenu();
@@ -72,5 +103,23 @@ function createWindow () {
             event.returnValue = false;
         }
     });
+}
+
+function contentWindow(url){
+    let newWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: false,
+            enableRemoteModule: false,
+        }
+    });
+
+    newWindow.removeMenu();
+    newWindow.loadURL(url,  userAgent);
+
+    newWindow.on('closed', function() {
+        newWindow = null
+    })
 }
 
